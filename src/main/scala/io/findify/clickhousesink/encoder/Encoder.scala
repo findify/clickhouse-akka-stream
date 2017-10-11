c689aafbc73d88b1f3d1d716fc91f2b92766d22d
Grebennikov Roman <grv@dfdx.me>
2017-10-11 14:26:08 +0300

    compile fix

--- src/main/scala/io/findify/clickhousesink/encoder/Encoder.scala
+++ src/main/scala/io/findify/clickhousesink/encoder/Encoder.scala
@@ -5,5 +5,5 @@ import io.findify.clickhousesink.field.{Field, ScalarField}
 trait Encoder[T] {
   def encode(name: String, value: T): Seq[Field]
   def ddl(name: String): String
-  def ddl(table:String, options: String) = s"CREATE TABLE $table (${ddl("root")}) $options"
+  def ddl(table:String, options: String):String = s"CREATE TABLE $table (${ddl("root")}) $options"
 }

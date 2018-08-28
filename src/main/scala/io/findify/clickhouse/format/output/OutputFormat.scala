e90cb88108b300b0242cc30eb8fef1c55f6ab9b9
Grebennikov Roman <grv@dfdx.me>
2018-08-28 16:42:36 +0300

    make OutputFormat serializable

--- src/main/scala/io/findify/clickhouse/format/output/OutputFormat.scala
+++ src/main/scala/io/findify/clickhouse/format/output/OutputFormat.scala
@@ -4,7 +4,7 @@ import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 import io.findify.clickhouse.format.Field.Row
 
-trait OutputFormat extends LazyLogging {
+trait OutputFormat extends Serializable with LazyLogging {
   def name: String
   def write(value: Row): ByteString
 }
